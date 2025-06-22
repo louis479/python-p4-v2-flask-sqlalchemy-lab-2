d5c9705b48abdd2f28b66d0a82ab94b4ea6230d3
louis-ngugi <louisngugi858@gmail.com>
2025-06-22 22:22:29 +0300

    learnt sqlalchemy

--- server/models.py
+++ server/models.py
@@ -11,22 +11,85 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(SerializerMixin, db.Model):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship("Review", back_populates="customer")
+
+    items = association_proxy('reviews', 'item')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'reviews': [review.to_dict() for review in self.reviews]  # Serializing reviews
+        }
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'reviews': [review.serialize() for review in self.reviews if review.customer_id == self.id]
+        }
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(SerializerMixin, db.Model):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship("Review", back_populates="item")
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'reviews': [review.serialize() for review in self.reviews if review.item_id == self.id]
+        }
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'reviews': [review.to_dict() for review in self.reviews]  # Serializing reviews
+        }
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship("Customer", back_populates="reviews")
+    item = db.relationship("Item", back_populates="reviews")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'comment': self.comment,
+            'customer': self.customer.to_dict() if self.customer else None,  # Serialize customer object
+            'item': self.item.to_dict() if self.item else None  # Serialize item object
+        }
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'comment': self.comment,  # Ensure the comment is serialized correctly
+            'customer_id': self.customer_id,
+            'item_id': self.item_id
+        }
